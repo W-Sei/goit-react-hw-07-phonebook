8d9cb1b31a448292508ef196bf928b4b8bef2e25
Hanna Franko <sei.wrcl@gmail.com>
2023-04-13 21:22:45 +0300

    progress

--- src/components/Wrapper/Wrapper.styled.js
+++ src/components/Wrapper/Wrapper.styled.js
@@ -12,4 +12,4 @@ export const Wrapper = styled.div`
   border-radius: 20px;
   box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset,
     rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset;
-`;
+ `;
